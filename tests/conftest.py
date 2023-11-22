0a94cdd81897462ab95b2ad8b1c74b0c47102ab5
ilya.st <ilya.st@halykmarket.com>
2023-11-22 10:36:56 +0600

    added tg notification

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,13 +15,14 @@ BrowserType = Literal['chrome', 'firefox', 'edge']
 
 
 class Configure(pydantic_settings.BaseSettings):
-    context: Literal['local', 'prod'] = 'prod'
+    context: Literal['local', 'prod', 'stage'] = 'stage'
     base_url: str = 'https://demoqa.com'
     height: str = '1080'
     width: str = '1920'
     timeout: float = 3.0
     browser: BrowserType = 'chrome'
-    version: str = '100'
+    version: str = '119'
+    run_remote: bool = True
     login: str = 'dummy_value'
     password: str = 'dummy_value'
     remote_browser_url: str = 'dummy_value'
@@ -29,32 +30,42 @@ class Configure(pydantic_settings.BaseSettings):
 
 config = Configure(_env_file=path(f'.env.{Configure().context}'))
 
+
 @pytest.fixture(scope='function', autouse=True)
 def configure_browser():
-
     options = Options()
-    selenoid_capabilities = {
-        'browserName': config.browser,
-        'browserVersion': config.version,
-        'selenoid:options': {
-            'enableVNC': True,
-            'enableVideo': True
+    if config.run_remote:
+        selenoid_capabilities = {
+            'browserName': config.browser,
+            'browserVersion': config.version,
+            'selenoid:options': {
+                'enableVNC': True,
+                'enableVideo': True
+            }
         }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    options.add_argument(f'--window-size={config.width},{config.height}')
-    driver = webdriver.Remote(
-        command_executor=f"https://{config.login}:{config.password}@{config.remote_browser_url}",
-        options=options
-    )
+        options.capabilities.update(selenoid_capabilities)
+        options.add_argument(f'--window-size={config.width},{config.height}')
+
+        driver = webdriver.Remote(
+            command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+            options=options
+        )
+        browser.config.driver = driver
+    else:
+        browser.config.driver_name = config.browser
+        options.add_argument('--ignore-certificate-errors')
+        browser.config.driver_options = options
+        browser.config.window_width = config.width
+        browser.config.window_height = config.height
+
     browser.config.base_url = config.base_url
-    browser.config.driver = driver
 
     yield
 
     attach.add_screenshot(browser)
     attach.add_html(browser)
-    #attach.add_logs(browser)
+    if config.browser == 'chrome':
+        attach.add_logs(browser)
     attach.add_video(browser)
 
     browser.quit()
